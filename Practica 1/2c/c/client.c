196b55f5cab37bd916da739f7d6ca37c1f1a558c
Marianela Diaz Catan <diazcatanm@gmail.com>
2023-08-29 10:18:50 -0300

    2c

---
+++ Practica 1/2c/c/client.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>  
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define buf_size 25	
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[buf_size];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+	//TOMA EL NUMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+	//CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+	//TOMA LA DIRECCION DEL SERVER DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+	//COPIA LA DIRECCION IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+     serv_addr.sin_port = htons(portno);
+	
+	//DESCRIPTOR - DIRECCION - TAMAÑO DIRECCION
+    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+        
+    //printf("Please enter the message: ");
+    
+    bzero(buffer,buf_size);
+    
+    //fgets(buffer,buf_size-1,stdin);
+    memset((buffer), 'a', buf_size);
+    
+    //GENERA SDBM HASH
+    unsigned int hash = 0;
+    for (int i = 0; i < strlen(buffer); i++)
+    	hash = buffer[i] + (hash << 6) + (hash << 16) - hash;
+    
+    printf("hash: %d\n", hash);
+    
+    //ENVIA CANTIDAD DE BYTES DEL MENSAJE AL SOCKET
+    n = write(sockfd,strlen(buffer),sizeof(strlen(buffer)));
+    if (n < 0) 
+         error("ERROR writing cant bytes message to socket");
+         
+    //ESPERA RECIBIR UNA RESPUESTA
+	n = read(sockfd,buffer,buf_size-1);
+
+    if (n < 0) 
+ 	error("ERROR reading from socket");
+	
+    //ENVIA UN MENSAJE AL SOCKET
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing message to socket");
+    bzero(buffer,buf_size);
+    
+    //ESPERA RECIBIR UNA RESPUESTA
+    n = read(sockfd,buffer,buf_size-1);
+    if (n < 0) 
+         error("ERROR reading from socket");
+        
+    
+    //ENVIA CANTIDAD DE BYTES DEL HASH AL SOCKET
+    n = write(sockfd,hash,sizeof(hash));
+    if (n < 0) 
+         error("ERROR writing to socket");
+         
+    //ESPERA RECIBIR UNA RESPUESTA
+    n = read(sockfd,buffer,buf_size-1);
+    	
+    //ENVIA HASH AL SOCKET
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,buf_size);
+
+    //ESPERA RECIBIR UNA RESPUESTA
+	n = read(sockfd,buffer,buf_size-1);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+	printf("%s\n",buffer);
+    return 0;
+}
---
+++ Practica 1/2c/c/server.c
@@ -0,0 +1,119 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>  
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define buf_size 25
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}	
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno, clilen;
+     char buffer[buf_size];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+	 //CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	 //AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//SOCK_STREAM - TIPO DE SOCKET 
+	
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     //ASIGNA EL PUERTO PASADO POR ARGUMENTO
+	 //ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+	 portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+	 
+	 //VINCULA EL FILE DESCRIPTOR CON LA DIRECCION Y EL PUERTO
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+			  
+	 //SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXION		  
+     listen(sockfd,5);
+	 
+	 // SE BLOQUEA A ESPERAR UNA CONEXION
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+				 
+     //DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
+	 if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,buf_size);
+     
+     //LEE CANT DE BYTES QUE RECIBIRA
+     n = read(newsockfd,buffer,buf_size);
+     int cant_bytes = (unsigned int)buffer;
+     
+     bzero(buffer,buf_size);
+     
+     //RESPONDE AL CLIENTE
+     n = write(newsockfd,"I got your message",18);
+
+     //LEE EL MENSAJE DEL CLIENTE
+     int j = 0;
+     do{
+     	n = read(newsockfd,buffer,buf_size);
+     	if (n < 0){ 
+     	  error("ERROR reading from socket");
+     	  break;
+     	}
+     	j += n;
+     } while(j < cant_bytes);
+     
+     printf("Cantidad de caracteres leidos: %d\n",n);
+     
+    //GENERA SDBM HASH
+    unsigned int hash = 0;
+    for (int i = 0; i < strlen(buffer); i++)
+    	hash = buffer[i] + (hash << 6) + (hash << 16) - hash;
+    
+    printf("hash: %d\n", hash);
+    
+    bzero(buffer,buf_size);
+     
+     //LEE CANT DE BYTES QUE RECIBIRA HASH
+     n = read(newsockfd,buffer,buf_size);
+     cant_bytes = (unsigned int)buffer;
+     
+     //LEE EL MENSAJE DEL CLIENTE
+     	j = 0;
+     do{
+     	n = read(newsockfd,buffer,buf_size);
+     	if (n < 0){ 
+     	  error("ERROR reading from socket");
+     	  break;
+     	}
+     	j += n;
+     } while(j < cant_bytes);
+     
+     //RESPONDE AL CLIENTE
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     
+
+     if (n < 0) error("ERROR reading from socket");
+
+     if(hash != (unsigned int)buffer) printf("El mensaje fue alterado");	 
+
+     return 0; 
+}
