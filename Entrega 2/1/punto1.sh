7129d37a2fb2b7ddcc2ea30fd5cf312603d3753e
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-09-18 11:15:09 -0300

    Tiempo tomado con C

---
+++ Entrega 2/1/punto1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#----------------------client-------------------------------
+
+
+port=3999
+for size in 1000 10000 100000 1000000
+do
+	echo "--------------------------------------------" >> ./tiempo1.txt
+	echo "tamaño de la entrada $size" >> ./tiempo1.txt
+	echo "--------------------------------------------" >> ./tiempo1.txt
+	for i in 1 2 3 4 5 6 7 8 9 10
+	do
+		port=$((port + 1))
+		./cliente 163.10.54.186 $port $size >> ./tiempo1.txt
+	done
+	sleep 3
+done
