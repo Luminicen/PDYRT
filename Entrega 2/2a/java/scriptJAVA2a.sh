b1e5ccf45572b29f5fe17d740bf7343e8289d3c4
Marianela Diaz Catan <diazcatanm@gmail.com>
2023-09-23 17:28:41 -0300

    java

--- Entrega 2/2a/java/scriptJAVA2a.sh
+++ Entrega 2/2a/java/scriptJAVA2a.sh
@@ -8,18 +8,21 @@ vagrant up
 #inicia 5 conexiones server desde vm1
 #podria poner otro for mas afuera con el tamaño de buffer pero dsp pruebo
 
-port=3999
+port=4999
 for size in 1000 10000 100000 1000000
 do
-	echo "--------------------------------------------" >> ./tiempoJAVA2a.txt
-	echo "tamaño de la entrada $size" >> ./tiempoJAVA2a.txt
-	echo "--------------------------------------------" >> ./tiempoJAVA2a.txt
-	for i in 1 2 3 4 5 6 7 8 9 10
-	do
-		port=$((port + 1))
-		echo $port
-		vagrant ssh vm1 -c "cd /vagrant; java Server.java $port" &
-		vagrant ssh vm2 -c "cd /vagrant; java Client.java 192.168.1.45 $port $size >> /vagrant/tiempoJAVA2a.txt"
-	done
-	sleep 3
+echo "--------------------------------------------" >> ./tiempoJAVA2a.txt
+echo "tamaño de la entrada $size" >> ./tiempoJAVA2a.txt
+echo "--------------------------------------------" >> ./tiempoJAVA2a.txt
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+port=$((port + 1))
+printf "\n"
+echo $port
+printf "\n"
+echo $1
+printf "\n"
+vagrant ssh vm1 -c "cd /vagrant; java Server.java $port" &
+vagrant ssh vm2 -c "cd /vagrant; java Client.java 192.168.1.45 $port $size >> /vagrant/tiempoJAVA2a.txt"
+done
 done
