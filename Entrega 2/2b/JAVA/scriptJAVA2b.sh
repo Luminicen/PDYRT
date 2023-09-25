76a44c7b783b88ede2881b88dd47b3f5da6f235c
Marianela Diaz Catan <diazcatanm@gmail.com>
2023-09-25 19:58:11 -0300

    modifica para ip dinamica y agrega tiempos de java

--- Entrega 2/2b/JAVA/scriptJAVA2b.sh
+++ Entrega 2/2b/JAVA/scriptJAVA2b.sh
@@ -2,10 +2,10 @@
 
 #levanta la vm en el directorio actual (provider el file de server.c y client.c)
 vagrant up
-ip=vagrant ssh vm -c "ip address" | 
+ip=$(vagrant ssh -c "hostname -I" | awk '{print $2}') 
 
 
-port=4999
+port=5999
 for size in 1000 10000 100000 1000000
 do
 echo "--------------------------------------------" >> ./tiempoJAVA2b.txt
@@ -17,12 +17,8 @@ port=$((port + 1))
 printf "\n"
 echo $port
 printf "\n"
-vagrant ssh vm -c "cd /vagrant; java Server.java $port" &
-done
-port=4999
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-port=$((port + 1))
-java Client.java 192.168.1.45 $port $size >> /vagrant/tiempoJAVA2b.txt
+vagrant ssh -c "cd /vagrant; java Server.java $port" &
+sleep 10
+java Client.java $ip $port $size >> ./tiempoJAVA2b.txt
 done
 done
