f20ee00afa6dff2ce2ff0a3747f2fcf6b5d1ff31
Juliana Luminicen Delle Ville <julianadelleville@hotmail.com>
2023-10-14 16:15:01 -0300

    puto proto

---
+++ Entrega 3/4/FTPServiceImpl.java
@@ -0,0 +1,75 @@
+package pdytr.ftp.grpc;
+
+import java.io.RandomAccessFile;
+import java.io.FileNotFoundException;
+import io.grpc.stub.StreamObserver;
+
+public class FTPServiceImpl extends FtpServiceGrpc.FtpServiceImplBase {
+  @Override
+  public void read(FTPService.ReadRequest request,
+        StreamObserver<FTPService.ReadResponse> responseObserver) {
+
+    try{
+      //crea un buffer del tamaño de datps cantidad a leer
+      byte[] buffer = new byte[(Integer)request.getOffset()];
+      int readBytes;
+      RandomAccessFile rfile = new RandomAccessFile(request.getName(), "r");
+      
+        //rfile.seek(request.getPosition);
+
+        //supestamente lee hasta completar el buff sino usar read(bytes[], off,len)
+        readBytes = rfile.read(buffer,(Integer)request.getPosition(),(Integer)request.getOffset());
+
+        //leyo hasta el final del archivo
+        if(readBytes == -1){
+          readBytes = rfile.length() - request.getOffset;
+        }
+
+      
+
+      // You must use a builder to construct a new Protobuffer object
+    FTPService.ReadResponse response = FTPService.ReadResponse.newBuilder()
+      .setContent(buffer)
+      .setRequestedReadBytes(request.getRequestedReadBytes)
+      .setReadBytes(readBytes)
+      .build();
+
+    // Use responseObserver to send a single response back
+    responseObserver.onNext(response);
+    }
+    catch(FileNotFoundException e){
+      System.out.println("El archivo solicitado no existe");
+      e.printStackTrace();
+    }
+    finally{
+      if (file != null){
+        file.close();
+      }
+    }
+
+    
+
+    // When you are done, you must call onCompleted.
+    responseObserver.onCompleted();
+  }
+
+  public void write(FTPService.WriteRequest request,
+        StreamObserver<FTPService.WriteResponse> responseObserver) {
+    
+    // HelloRequest has toString auto-generated.
+    System.out.println(request);
+
+    // You must use a builder to construct a new Protobuffer object
+    FTPService.WriteResponse response = FTPService.WriteResponse.newBuilder()
+      .setName(request.getName)
+      .setOffset(request.getOffset)
+      .setBuffer(request.getBuffer)
+      .build();
+
+    // Use responseObserver to send a single response back
+    responseObserver.onNext(response);
+
+    // When you are done, you must call onCompleted.
+    responseObserver.onCompleted();
+  }
+}
