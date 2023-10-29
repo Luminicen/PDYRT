5df7706df7b4f94ac70a2b7844d5b49d3d3278b1
Marianela Diaz Catan <diazcatanm@gmail.com>
2023-10-29 12:09:58 -0300

    mod para prueba con binarios

--- Entrega 3/4/Servidor con binarios/ftp/script.sh
+++ Entrega 3/4/Servidor con binarios/ftp/script.sh
@@ -4,16 +4,11 @@
 mvn -DskipTests package exec:java -Dexec.mainClass=pdytr.ftp.grpc.App --quiet &
 s_pid=$!
 sleep 3
-a=$(cat ./ClienteA.txt)
-b=$(cat ./ClienteB.txt)
-#inicia cliente
-mvn -DskipTests exec:java -Dexec.mainClass=pdytr.ftp.grpc.Client -Dexec.args="w ./db/experimentoB.txt 5 $a" &
-ca_pid=$!
-mvn -DskipTests exec:java -Dexec.mainClass=pdytr.ftp.grpc.Client -Dexec.args="w ./db/experimentoB.txt 5 $b" &
-cb_pid=$!
 
-wait $ca_pid
-wait $cb_pid
+#inicia cliente
+mvn -DskipTests exec:java -Dexec.mainClass=pdytr.ftp.grpc.Client -Dexec.args="f ./db/reciboFatality.mp4 119487 ./fatality.mp4"
+
+diff -s ./db/reciboFatality.mp4 ./fatality.mp4
 
 kill $s_pid
 
